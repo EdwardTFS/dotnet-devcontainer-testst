20becd2150504cd7103f1ac3c40603153aca6152
Edward Görlich <goerlich_e@tfs.pl>
2024-10-23 21:18:42 +0000

    program class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-
-Console.WriteLine("Hello, World!");
+static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
